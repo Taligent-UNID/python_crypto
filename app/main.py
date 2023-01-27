ed3ac8368b88dc5e625e591c48241b8de2094515
Kevin Barroso <kobo.devops@gmail.com>
2023-01-27 15:13:16 -0300

    frame3

--- app/main.py
+++ app/main.py
@@ -2,8 +2,8 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
 from tkinter import messagebox as mb
-from rsa_encryption import create_pair_keys
-
+from rsa_encryption import create_pair_keys,generate_password
+import pyperclip as pc
 
 class MainApp(tk.Tk):
     def __init__(self):
@@ -56,10 +56,9 @@ class MainApp(tk.Tk):
             self.frame1,
             text='Generate Key Pair',
             style='Headings.TLabel'
-        )
-        self.label_frame1.pack()
+        ).pack()
 
-        def filename_path():
+        def filename_path_frame1():
             filename = filedialog.askdirectory()
             if filename:
                 self.label_directory_frame1.config(text=filename)
@@ -68,10 +67,9 @@ class MainApp(tk.Tk):
 
         self.button_directory_frame1 = ttk.Button(
             self.frame1,
-            text='Select Directory..',
-            command=filename_path
-        )
-        self.button_directory_frame1.pack()
+            text='Save keys pair..',
+            command=filename_path_frame1
+        ).pack()
 
         self.label_directory_frame1 = ttk.Label(
             self.frame1,
@@ -90,46 +88,99 @@ class MainApp(tk.Tk):
             text='Generate',
             state='disabled',
             command=generate_keys_message
+        ).pack()
+
+        # Frame 2 Generate Password Random
+        self.frame2 = ttk.Frame(self)
+        self.frame2.config(width=400,height=400)
+        self.frame2.grid(row=1,column=1)
+
+        self.title_frame2 = ttk.Label(
+            self.frame2,
+            text='Generate Password Random',
+            style='Headings.TLabel'
+        ).pack()
+
+        self.label_password_frame2 = ttk.Label(
+            self.frame2,
+            text='Password generated random...'
         )
-        self.button_generate_frame1.pack()
 
-        # Frame 2 
-        
-        
-        
-        
+        def generate_password_label():
+            password = generate_password()
+            self.label_password_frame2.config(text=password)
+            pc.copy(password)
+            mb.showinfo('SDC','Password generada y copiada...')
 
-        
-        
-        
-        
-        
-        
+        self.button_generate_pass_frame2 = ttk.Button(
+            self.frame2,
+            text='Generate',
+            command=generate_password_label
+        ).pack()
 
-        
-        
-        
-        
-        
+        self.label_password_frame2.pack()
 
-        
-        
-        
+        self.button_copy_pass_frame2 = ttk.Button(
+            self.frame2,
+            text='Copy',
+            command=lambda: pc.copy(self.label_password_frame2['text'])
+        ).pack()
+    
+        # Frame 3 Encrypt Password
+        self.frame3 = ttk.Frame(self)
+        self.frame3.config(width=400,height=400)
+        self.frame3.grid(row=2,column=0)
 
-        
-        
-        
-        
-        
-        
+        self.title_frame3 = ttk.Label(
+            self.frame3,
+            text='Generate File Encrypt',
+            style='Headings.TLabel'
+        ).pack()
 
-        
-        
-        
-        
+        def filename_path_frame3():
+            filename = filedialog.askdirectory()
+            if filename:
+                self.label_directory_frame3.config(text=filename,textvariable=filename)
+                if self.label_private_key_frame3['textvariable']:
+                    self.button_generate_frame3.config(state='enabled')
+                self.filename = filename
 
+        self.button_directory_frame3 = ttk.Button(
+            self.frame3,
+            text='Save file encrypt..',
+            command=filename_path_frame3
+        ).pack()
 
+        self.label_directory_frame3 = ttk.Label(
+            self.frame3,
+            text='Path Save as file encrypt..'
+        )
+        self.label_directory_frame3.pack()
 
+        def search_private_key():
+            filename = filedialog.askopenfilename()
+            self.label_private_key_frame3.config(text=filename,textvariable=filename)
+            if self.label_directory_frame3['textvariable']:
+                self.button_private_key_frame3.config(state='enabled')
+
+        self.button_private_key_frame3 = ttk.Button(
+            self.frame3,
+            text='Select private key...',
+            command=search_private_key
+        )
+        self.button_private_key_frame3.pack()
+
+        self.label_private_key_frame3 = ttk.Label(
+            self.frame3,
+            text='Path private key...'
+        )
+        self.label_private_key_frame3.pack()
+
+        self.button_generate_frame3 = ttk.Button(
+            self.frame3,
+            text='Generate',
+            state='disabled'
+        ).pack()
 
 
 if __name__ == '__main__':
