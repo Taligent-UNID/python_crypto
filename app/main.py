0d467aba7af1ca97295c5209cbbfe430295d09e9
Kevin Barroso <kobounix@DESKTOP-K0CS228>
2023-01-27 18:23:14 -0300

    styles

--- app/main.py
+++ app/main.py
@@ -12,13 +12,24 @@ class MainApp(tk.Tk):
 
         # UI options
         paddings = {'padx': 5, 'pady': 10}
+        padTitles = {'padx': 10,'pady': 5}
 
         # configure the root window
         self.title('Secure Delivery of Credentials - Taligent')
-        self.geometry('800x800')
         self.config(bg='white')
         self.iconbitmap('app/images/taligent.ico')
-
+        self.resizable(False, False) 
+        window_height = 500
+        window_width = 500
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+        x_cordinate = int((screen_width/2) - (window_width/2))
+        y_cordinate = int((screen_height/2) - (window_height/2))
+        self.geometry("{}x{}+{}+{}".format(window_width, window_height, x_cordinate, y_cordinate))
+        
+        # layout all of the main containers
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_columnconfigure(0, weight=1)
         # Styles
         self.style = ttk.Style(self)
         self.style.theme_use('clam')
@@ -37,6 +48,17 @@ class MainApp(tk.Tk):
         self.style.configure(
             'Headings.TLabel',
             font=('Roboto 15'),
+            background='white'
+        )
+        self.style.configure(
+            'Subheadings.TLabel',
+            font=('Roboto 11'),
+            background='white'
+        )
+        # Style Frames
+        self.style.configure(
+            'TFrame',
+            background='white'
         )
 
         # Label
@@ -50,14 +72,14 @@ class MainApp(tk.Tk):
 
         # Frame 1 Generate Key Pair
         self.frame1 = ttk.Frame(self)
-        self.frame1.config(width=400,height=400)
-        self.frame1.grid(row=1,column=0)
+        self.frame1.config(width=250,height=250)
+        self.frame1.grid(row=1,column=0,sticky='ne')
 
         self.label_frame1 = ttk.Label(
             self.frame1,
             text='Generate Key Pair',
             style='Headings.TLabel'
-        ).pack()
+        ).pack(**padTitles)
 
         def filename_path_frame1():
             filename = filedialog.askdirectory()
@@ -74,7 +96,8 @@ class MainApp(tk.Tk):
 
         self.label_directory_frame1 = ttk.Label(
             self.frame1,
-            text='Path Save As Keys..'
+            text='Path Save As Keys..',
+            style='Subheadings.TLabel'
         )
         self.label_directory_frame1.pack()
 
@@ -94,18 +117,19 @@ class MainApp(tk.Tk):
 
         # Frame 2 Generate Password Random
         self.frame2 = ttk.Frame(self)
-        self.frame2.config(width=400,height=400)
-        self.frame2.grid(row=1,column=1)
+        self.frame2.config(width=250,height=250)
+        self.frame2.grid(row=1,column=1,sticky='nw')
 
         self.title_frame2 = ttk.Label(
             self.frame2,
             text='Generate Password Random',
             style='Headings.TLabel'
-        ).pack()
+        ).pack(**padTitles)
 
         self.label_password_frame2 = ttk.Label(
             self.frame2,
-            text='Password generated random...'
+            text='Password generated random...',
+            style='Subheadings.TLabel'
         )
 
         def generate_password_label():
@@ -130,14 +154,14 @@ class MainApp(tk.Tk):
     
         # Frame 3 Encrypt Password
         self.frame3 = ttk.Frame(self)
-        self.frame3.config(width=400,height=400)
-        self.frame3.grid(row=2,column=0)
+        self.frame3.config(width=250,height=250)
+        self.frame3.grid(row=2,column=0,sticky='se')
 
         self.title_frame3 = ttk.Label(
             self.frame3,
             text='Generate File Encrypt',
             style='Headings.TLabel'
-        ).pack()
+        ).pack(**padTitles)
 
         def filename_path_frame3():
             filename = filedialog.askdirectory()
@@ -153,7 +177,8 @@ class MainApp(tk.Tk):
 
         self.label_directory_frame3 = ttk.Label(
             self.frame3,
-            text='Path Save as file encrypt..'
+            text='Path Save as file encrypt..',
+            style='Subheadings.TLabel'
         )
         self.label_directory_frame3.pack()
 
@@ -170,7 +195,8 @@ class MainApp(tk.Tk):
 
         self.label_public_key_frame3 = ttk.Label(
             self.frame3,
-            text='Path public key...'
+            text='Path public key...',
+            style='Subheadings.TLabel'
         )
         self.label_public_key_frame3.pack()
 
@@ -222,13 +248,13 @@ class MainApp(tk.Tk):
 
         # Frame 4 Decrypt File
         self.frame4 = ttk.Frame(self)
-        self.frame4.config(width=400,height=400)
-        self.frame4.grid(row=2,column=1)
+        self.frame4.config(width=250,height=250)
+        self.frame4.grid(row=2,column=1,sticky='sw')
         self.title_frame4 = ttk.Label(
             self.frame4,
             text='Generate Decrypt Password',
             style='Headings.TLabel'
-        ).pack()
+        ).pack(side='top',**padTitles)
 
         def filename_path_frame4():
             filename = filedialog.askopenfilename()
@@ -244,7 +270,8 @@ class MainApp(tk.Tk):
 
         self.label_directory_frame4 = ttk.Label(
             self.frame4,
-            text='Path file encrypt..'
+            text='Path file encrypt..',
+            style='Subheadings.TLabel'
         )
         self.label_directory_frame4.pack()
 
@@ -260,13 +287,15 @@ class MainApp(tk.Tk):
 
         self.label_private_key_frame4 = ttk.Label(
             self.frame4,
-            text='Path private key...'
+            text='Path private key...',
+            style='Subheadings.TLabel'
         )
         self.label_private_key_frame4.pack()
 
         self.label_password_frame4 = ttk.Label(
             self.frame4,
-            text='Password decrypt...'
+            text='Password decrypt...',
+            style='Subheadings.TLabel'
         )
         def generate_decrypt_password():
             validate = []
