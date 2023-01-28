25b51465a002d5bb1f3d64b9b707e0638cd36bee
Kevin Barroso <kobounix@DESKTOP-K0CS228>
2023-01-28 09:44:43 -0300

    readme app

---
+++ app/README.md
@@ -0,0 +1,24 @@
+# Aplication SDC
+
+## Objetive
+
+La aplicacion tiene como objetivo un envio seguro de credenciales a traves de un cifrado asimetrico.
+
+### Funcionalidades:
+
+- Generacion de claves pares
+- Encriptacion de texto
+- Des encriptacion de texto
+- Generacion de password random
+
+## Enviroment
+
+Python 3.9
+
+### Requirements
+
+- Cryptography 39.0.0
+- Tkinter (Nativo)
+- Pathlib (Nativo)
+- Pyperclip 1.8.2
+
--- app/rsa_encryption.py
+++ app/rsa_encryption.py
@@ -10,7 +10,7 @@ import random, string
 def utf8(s: bytes):
     return str(s, 'utf-8')
 
-# Crea las claves pares en la carpeta "keys"
+# Crea las claves pares en la carpeta seleccionada
 def create_pair_keys(filename):
     private_key = rsa.generate_private_key(
        public_exponent=65537,
@@ -36,6 +36,7 @@ def create_pair_keys(filename):
     with open(f'{filename}/public_key.pem', 'wb') as f:
         f.write(public_pem)
 
+# Encripta la password y genera el archivo cifrado
 def encrypt_password(public_key,password,filename_encrypt):
     with open(public_key, "rb") as key_file:
         public_key = serialization.load_pem_public_key(
@@ -54,6 +55,7 @@ def encrypt_password(public_key,password,filename_encrypt):
     with open(f'{filename_encrypt}/file_encrypted.txt', 'wb') as f:
         f.write(encrypted)
 
+# Des encripta el archivo cifrado y retornando la password
 def decrypt_password(path_private_key,file_encrypted):
 
     with open(path_private_key, "rb") as key_file:
@@ -77,7 +79,7 @@ def decrypt_password(path_private_key,file_encrypted):
 
     return decrypted
 
-
+# Genera una password random alphanumerica de 20 caracteres
 def generate_password():
     length = 20
     chars = string.ascii_letters + string.digits + '!@#$%^&*()'
